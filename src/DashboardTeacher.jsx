e5d42283d1ebac8aad2a3a5c8288a6b77edf01bc
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-11-01 22:54:11 +0200

    Setting uo teacherDash

---
+++ src/DashboardTeacher.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DashboardSidebar from './DashboardTeacher/DashboardSidebar'
+
+const DashboardTeacher = () => {
+  return (
+    <div>
+      <DashboardSidebar/>
+    </div>
+  )
+}
+
+export default DashboardTeacher
---
+++ src/DashboardTeacher/DashboardSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashboardSidebar = () => {
+  return (
+    <div>
+      <h1>sidebar</h1>
+    </div>
+  )
+}
+
+export default DashboardSidebar
