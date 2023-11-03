2228cfb3e40a70359cef0d79ed0a4caf2d0471f3
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-11-03 23:53:39 +0200

    Fixed enrollment

---
+++ src/Student/EnrollmentForm .jsx
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../CSS/Student.css';
+
+function EnrollmentForm({ userId }) {
+  const [languages, setLanguages] = useState([]);
+  const [levels, setLevels] = useState([]);
+  const [selectedLanguage, setSelectedLanguage] = useState('');
+  const [selectedLevel, setSelectedLevel] = useState('');
+  const [levelId, setLevelId] = useState('');
+
+  useEffect(() => {
+   // Fetch available languages and populate the language dropdown
+axios.get('http://localhost:5000/enroll/get/languages')
+.then((response) => {
+  setLanguages(response.data.languages);
+})
+.catch((error) => {
+  console.error(error);
+});
+  }, []);
+
+  const handleLanguageChange = (event) => {
+    const selectedLanguageId = event.target.value;
+    setSelectedLanguage(selectedLanguageId);
+
+// Fetch available levels for the selected language and populate the level dropdown
+axios.get(`http://localhost:5000/enroll/get/levels?language_id=${selectedLanguageId}`)
+  .then((response) => {
+    setLevels(response.data.levels);
+    setLevelId(response.data.levels[0].level_id)
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+}
+
+  const handleEnroll = () => {
+      const studentId = localStorage.getItem("userId")
+      console.log(studentId)
+      const req = {
+        student_id : studentId ,
+        level_id : levelId
+      }
+      // Send a POST request to the backend API to enroll the student
+      fetch('http://localhost:5000/enroll/add/enroll', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+            },
+      body: JSON.stringify(req),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.message);
+        // Handle success or error messages as needed
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <div className='enrollment-div'>
+        {/* <h1 className='enroll-title'>What do you wanna learn</h1> */}
+      {/* <label className='select-language' htmlFor="language">Select Language:</label> */}
+      <select className='dropdown-language' id="language" name="language" value={selectedLanguage} onChange={handleLanguageChange}>
+        <option value="">Select a Language</option>
+        {languages && languages.map((language) => (
+          <option key={language.language_id} value={language.language_id}>
+            {language.language_name}
+          </option>
+        ))}
+      </select>
+
+      {/* <label className='select-level' htmlFor="level">Select Level:</label> */}
+      <select className='dropdown-levels' id="level" name="level" value={selectedLevel} onChange={(e) => setSelectedLevel(e.target.value)}>
+        <option value="">Select a Level</option>
+        {levels.map((level) => (
+          <option key={level.level_id} value={level.level_id}>
+            {level.level_name}
+          </option>
+        ))}
+      </select>
+
+      <button className='enroll-btn' type="button" onClick={handleEnroll} disabled={!selectedLanguage || !selectedLevel}>
+        Enroll
+      </button>
+    </div>
+  );
+}
+
+export default EnrollmentForm;
---
+++ src/Student/StartLearning.jsx
@@ -0,0 +1,194 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Modal from 'react-modal';
+import '../CSS/Student.css'
+
+function StartLearning({ userId }) {
+  const [languages, setLanguages] = useState([]);
+  const [levels, setLevels] = useState([]);
+  const [selectedLanguage, setSelectedLanguage] = useState('');
+  const [selectedLevel, setSelectedLevel] = useState('');
+  const [selectedLevelName, setSelectedLevelName] = useState('');
+  const [enrolledLevels, setEnrolledLevels] = useState([]);
+  const [lessons, setLessons] = useState([]);
+  const [assessmentDetails, setAssessmentDetails] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  useEffect(() => {
+    // Fetch available languages and populate the language dropdown
+    axios
+      .get('http://localhost:5000/enroll/get/languages')
+      .then((response) => {
+        setLanguages(response.data.languages);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  // Fetch enrolled levels when userId changes
+  useEffect(() => {
+    if (userId && selectedLanguage) {
+      // Fetch enrolled levels for the selected language
+      axios
+        .get(`http://localhost:5000/enroll/get/enrolledLevels`)
+        .then((response) => {
+          setEnrolledLevels(response.data);
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error('Error fetching enrolled levels:', error);
+        });
+    }
+  }, [userId, selectedLanguage]);
+
+  const handleLanguageChange = (event) => {
+    const selectedLanguageId = event.target.value;
+    setSelectedLanguage(selectedLanguageId);
+  };
+
+  const handleLevelChange = (event) => {
+    const selectedOption = event.target.options[event.target.selectedIndex];
+    setSelectedLevel(event.target.value);
+    setSelectedLevelName(selectedOption.text);
+  };
+
+  const handleStartLearning = () => {
+    axios
+      .get(`http://localhost:5000/students/lessons?language=${selectedLanguage}&level=${selectedLevelName}`)
+      .then((response) => {
+        setLessons(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching lessons:', error);
+      });
+  };
+
+
+  const [assessmentId, setAssessmentId] = useState(null);
+
+  const handleStartAssessment = (lessonId) => {
+    // Make an API request to fetch the assessment details for the selected lesson
+    axios
+      .get(`http://localhost:5000/students/fetch-assessment/${userId}/${lessonId}`)
+      .then((response) => {
+        // Handle the response and show the assessment details to the user
+        const assessmentDetails = response.data.data;
+        setAssessmentDetails(assessmentDetails);
+        console.log('Assessment details:', assessmentDetails);
+
+        // Store the assessment ID in state
+        setAssessmentId(assessmentDetails.assessment_id);
+
+        // You can display the assessment details using a modal or another component
+      })
+      .catch((error) => {
+        console.error('Error fetching assessment:', error);
+      });
+    setIsModalOpen(true);
+  };
+
+
+  const handleSubmitAssessment = () => {
+    if (assessmentId && userId) {
+      // Make an API request to create the user_assessment row and set submission to 'Submitted'
+      axios
+        .put('http://localhost:5000/students/submit-assessment', {
+          assessmentId: assessmentId,
+          studentId: userId,
+        })
+        .then((submitResponse) => {
+          console.log('Assessment submitted:', submitResponse.data);
+
+          // You can also update the submission status in the state if needed
+        })
+        .catch((submitError) => {
+          console.error('Error submitting assessment:', submitError);
+        });
+    } else {
+      console.error('Assessment ID or Student ID is missing. Please make sure to set both.');
+    }
+    document.querySelector('button[type="submit"]').disabled = true;
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  const [lessonIndex, setLessonIndex] = useState(0); 
+
+  const handleNextLesson = () => {
+    if (lessonIndex < lessons.length - 1) {
+      setLessonIndex(lessonIndex + 1);
+    }
+  };
+
+  const handlePreviousLesson = () => {
+    if (lessonIndex > 0) {
+      setLessonIndex(lessonIndex - 1);
+    }
+  };
+
+  const selectedLesson = lessons[lessonIndex];
+
+  return (
+    <div className='start-learning-div'>
+      <h2>Language and Level Selection</h2>
+      <label htmlFor="language">Select Language:</label>
+      <select id="language" name="language" value={selectedLanguage} onChange={handleLanguageChange}>
+        <option value="">Select a Language</option>
+        {languages.map((language) => (
+          <option key={language.language_id} value={language.language_id}>
+            {language.language_name}
+          </option>
+        ))}
+      </select>
+
+      <label htmlFor="level">Select Level:</label>
+      <select id="level" name="level" value={selectedLevel} onChange={handleLevelChange}>
+        <option value="">Select a Level</option>
+        {enrolledLevels.map((level) => (
+          <option key={level.level_id} value={level.level_name}>
+            {level.level_name}
+          </option>
+        ))}
+      </select>
+      <button onClick={handleStartLearning}>Start Learning</button>
+      {lessons.length > 0 && (
+        <div>
+          <h3>Available Lessons:</h3>
+          <ul>
+            {selectedLesson && (
+              <li key={selectedLesson.lesson_id}>
+                <h4>{selectedLesson.lesson_name}</h4>
+                <p>{selectedLesson.content}</p>
+                <button onClick={() => handleStartAssessment(selectedLesson.lesson_id)}>Start Assessment</button>
+              </li>
+            )}
+          </ul>
+
+          <div>
+            <button onClick={handlePreviousLesson} disabled={lessonIndex === 0}>
+              Previous Lesson
+            </button>
+            <button onClick={handleNextLesson} disabled={lessonIndex === lessons.length - 1}>
+              Next Lesson
+            </button>
+          </div>
+        </div>
+      )}
+
+      {isModalOpen && assessmentDetails && (
+        <Modal isOpen={isModalOpen} onClose={closeModal}>
+          <h3>Assessment Details:</h3>
+          <p>Assessment Title: {assessmentDetails.assessment_title}</p>
+          <p>Duration: {assessmentDetails.duration} minutes</p>
+          <p>Question: {assessmentDetails.question}</p>
+          <button type="submit" onClick={handleSubmitAssessment}>Submit Assessment</button>
+          <button onClick={closeModal}>Close</button>
+        </Modal>
+      )}
+
+
+    </div>
+  );
+}
+
+export default StartLearning;
