8e09e39e8ee8ba957c734cf03e7abcb76a3e2ad0
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-11-07 12:42:11 +0200

    got the assessment/ delete assessment:functional

--- src/DashboardTeacher/DashAssessment.jsx
+++ src/DashboardTeacher/DashAssessment.jsx
@@ -1,11 +1,123 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../CSS/DashboardTeacher.css';
 
-const DashAssessment = () => {
+const Dashassessment = () => {
+  const [selectedLevel, setSelectedLevel] = useState('');
+  const [lessons, setLessons] = useState([]);
+  const [assessments, setAssessments] = useState([]);
+  const [levels, setLevels] = useState([]);
+  const [selectedLesson, setSelectedLesson] = useState(null);
+
+  useEffect(() => {
+    // Fetch levels when the component mounts
+    axios.get(`http://localhost:5000/levels/getBylanguage/${localStorage.getItem('language_id')}`)
+      .then(response => {
+        setLevels(response.data.data);
+      })
+      .catch(error => {
+        console.error('Error fetching levels:', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (selectedLevel) {
+      // Fetch lessons when the selectedLevel changes
+      axios.get(`http://localhost:5000/lessons/getByLevel/${selectedLevel}`)
+        .then(response => {
+          setLessons(response.data.data);
+        })
+        .catch(error => {
+          console.error('Error fetching lessons:', error);
+        });
+    }
+  }, [selectedLevel]);
+
+  const handleLevelChange = (event) => {
+    const selectedLevelId = event.target.options[event.target.selectedIndex].id;
+    setSelectedLevel(selectedLevelId);
+    setSelectedLesson(null);
+  };
+  const handleLessonChange = (event) => {
+    const selectedLesson = event.target.options[event.target.selectedIndex].id;
+    setSelectedLesson(selectedLesson);
+    
+  }
+
+  useEffect(() => {
+    if (selectedLevel&&selectedLesson) {
+      // Fetch lessons when the selectedLevel changes
+      axios.get(`http://localhost:5000/assessment/getbylesson/${selectedLesson}`)
+        .then(response => {
+          setAssessments(response.data.data);
+        })
+        .catch(error => {
+          console.error('Error fetching lessons:', error);
+        });
+    }
+  }, [selectedLevel, selectedLesson]);
+
+  const handleDelete = (assessmentId) => {
+    const confirmDelete = window.confirm("Are you sure you want to delete this assessment?");
+    if (confirmDelete)
+    axios
+      .delete(`http://localhost:5000/assessment/delete/${assessmentId}`)
+      .then((response) => {
+        if (response.status === 204) {
+          // Handle successful deletion, e.g., updating the UI
+          setAssessments((prevAssessments) =>
+            prevAssessments.filter((assessment) => assessment.assessment_id !== assessmentId)
+          );
+        }
+      })
+      .catch((error) => {
+        console.error('Error deleting assessment:', error);
+      });
+  };
+  
+
+
   return (
     <div>
-      <h1>assessment</h1>
+      <h1>Assessments</h1>
+      <select name="level" id="levels" className="dropdown-level" onChange={handleLevelChange}>
+          <option value="Level">Level</option>
+          {levels.map((level) => (
+            <option key={level.level_id} value={level.level_name} id={level.level_id}>
+              {level.level_name}
+            </option>
+          ))}
+        </select>
+        <select name="lesson" id="lessons" className='dropdown-level' onChange={handleLessonChange}>
+          <option value="Lesson">Lesson</option>
+          {lessons.map((lesson) => (
+            <option key={lesson.lesson_id} value={lesson.lesson_name} id={lesson.lesson_id}>
+              {lesson.lesson_name}
+            </option>
+          ))}
+        </select>
+      <div className="assessments">
+        {assessments.map(assessment => (
+          <div className="assessment-row" key={assessment.id}>
+            <p className="list-of-assessments">{assessment.assessment_title}</p>
+            <img
+              className="svg-teach-dash"
+              src="./Images/sign-out-svgrepo-com.svg"
+              alt="Sign Out"
+            />
+           <img
+              src="./Images/bin-svgrepo-com.svg"
+              className="svg-teach-dash"
+              alt="Delete"
+              onClick={() => handleDelete(assessment.assessment_id)} // Pass the assessment ID here
+            />
+          </div>
+        ))}
+      </div>
+      <div className="line1"></div>
+      
     </div>
-  )
+  );
 }
 
-export default DashAssessment
+export default Dashassessment;
--- src/DashboardTeacher/DashboardSidebar.jsx
+++ src/DashboardTeacher/DashboardSidebar.jsx
@@ -32,50 +32,28 @@ function DashboardSidebar() {
               Profile
             </a>
             </div>
-            {visibleProfile && (
-              <div className='dropdown-content'>
-                <a href="#">- Name</a>
-                <a href="#">- Email</a>
-                <a href="#">- Languages</a>
-                <a href="#">- Levels</a>
-                <a href="#">- Image</a>
-              </div>
-            )}
+            
            <div className="elements">
            <img class="svg-teach-dash" src="../Images/open-book-svgrepo-com.svg"/>
             <a href="#" onClick={() => handleSidebarClick('lessons')}>
               Lessons
             </a>
             </div>
-            {visibleLessons && (
-              <div className='dropdown-content'>
-                <a href="#">- Title</a>
-                <a href="#">- Overview</a>
-                <a href="#">- Lesson</a>
-              </div>
-            )}
+            
             <div class="elements">
             <img class="svg-teach-dash" src="../Images/pdf-fill.svg"/>
             <a href="#" onClick={() => handleSidebarClick('assessment')}>
               Assessment
             </a>
             </div>
-            {visibleAssessment && (
-              <div className='dropdown-content'>
-                <a href="#">- Assessment</a>  
-              </div>
-            )}
+            
               <div class="elements">
              <img class="svg-teach-dash" src="../Images/user-check-alt-1-svgrepo-com.svg"/>
             <a href="#" onClick={() => handleSidebarClick('attendance')}>
               Attendance
             </a>
             </div>
-            {visibleAttendence && (
-              <div className='dropdown-content'>
-                <a href="#">- Attendance</a>
-              </div>
-            )}
+            
             
           </div>
           <div className="Content">
